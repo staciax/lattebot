fa9105cdc6d7f488fb3940682d2f0c5005ff499c
STACiA <69411527+staciax@users.noreply.github.com>
2024-11-10 02:26:44 +0700

    fix: update locale string import as `_T` to `_`

--- lattebot/cogs/test.py
+++ lattebot/cogs/test.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import discord
 from discord import app_commands
-from discord.app_commands import locale_str as _T
+from discord.app_commands import locale_str as _
 
 from lattebot.core.cog import LatteCog, context_menu
 
@@ -16,15 +16,15 @@ class Test(LatteCog, name='test'):
     def __init__(self, bot: LatteBot) -> None:
         self.bot: LatteBot = bot
 
-    @context_menu(name=_T('context_message'))
+    @context_menu(name=_('context_message'))
     async def test_context_message(self, interaction: discord.Interaction[LatteBot], message: discord.Message) -> None:
         await interaction.response.send_message(f'content: {message.content}', ephemeral=True)
 
-    @context_menu(name=_T('context_user'))
+    @context_menu(name=_('context_user'))
     async def test_context_user(self, interaction: discord.Interaction[LatteBot], user: discord.User) -> None:
         await interaction.response.send_message(f'username: {user.name}', ephemeral=True)
 
-    @app_commands.command(name=_T('command'), description=_T('command description'))
+    @app_commands.command(name=_('command'), description=_('command description'))
     async def test_command(self, interaction: discord.Interaction[LatteBot]) -> None:
         await interaction.response.send_message('command response', ephemeral=True)
 
