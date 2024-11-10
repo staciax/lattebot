482b954bf978caac6efa87f9916cd02ccffe3b6f
STACiA <69411527+staciax@users.noreply.github.com>
2024-11-11 06:01:23 +0700

    test: add unit tests for translator

---
+++ lattebot/tests/core/test_translator.py
@@ -0,0 +1,66 @@
+import pytest
+
+from lattebot.core.translator import (
+    AppCommand,
+    update_app_command_model,
+)
+
+
+@pytest.mark.anyio
+def test_update_app_command_data() -> None:
+    app_command = AppCommand.model_validate({
+        'name': 'a',
+        'description': 'a',
+        'options': {
+            'param_1': {
+                'display_name': 'p1',
+                'description': 'p1d',
+                'choices': {'1': '1', '2': '2', '3': '3'},
+            },
+            'param_2': {
+                'display_name': 'p2',
+                'description': 'p2d',
+            },
+        },
+    })
+
+    app_command_update = AppCommand.model_validate({
+        'name': 'b',
+        'description': 'b',
+        'options': {
+            'param_1': {
+                'display_name': 'p1x',
+                'description': 'p1dx',
+                'choices': {'1': '1x', '3': '3x', '4': '4'},
+            },
+            'param_2': {
+                'display_name': 'p2x',
+                'description': 'p2dx',
+                'choices': None,
+            },
+            'param_3': {
+                'display_name': 'p3',
+                'description': 'p3d',
+            },
+        },
+    })
+
+    updated_app_command = update_app_command_model(app_command, app_command_update)
+    updated_app_command_data = updated_app_command.model_dump()
+
+    assert updated_app_command_data == {
+        'name': 'b',
+        'description': 'b',
+        'options': {
+            'param_1': {
+                'display_name': 'p1x',
+                'description': 'p1dx',
+                'choices': {'1': '1x', '2': '2', '3': '3x'},
+            },
+            'param_2': {
+                'display_name': 'p2x',
+                'description': 'p2dx',
+                'choices': None,
+            },
+        },
+    }
