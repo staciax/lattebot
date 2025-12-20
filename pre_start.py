1abd4a4a931c741d854b78350b1dd09f178ada0b
STACIA <69411527+staciax@users.noreply.github.com>
2025-12-20 17:20:39 +0700

    chore: enhance translator state management (#11)
    * chore: enhance state management in Translator class
    
    * chore: add dry-run option for translator generation
    
    * chore: remove unnecessary readiness check in Translator unload method
    
    * chore: improve error handling in Translator's wait_until_ready method
    
    * chore: enhance readiness check in Translator's is_ready method
    
    * style: format
    
    * chore: enhance is_ready method to check if loading task is done

--- pre_start.py
+++ pre_start.py
@@ -1,19 +1,33 @@
 import asyncio
 import logging
+from argparse import ArgumentParser
 
 from lattebot.core.bot import LatteBot
 from lattebot.logging import setup_logging
 
 
 async def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--dry-run',
+        action='store_true',
+        help='Simulate translator generation without syncing commands to Discord',
+    )
+
+    args = parser.parse_args()
+
     with setup_logging(logging.DEBUG):
         async with LatteBot() as bot:
 
             @bot.event
             async def on_ready() -> None:
                 await bot.translator.wait_until_ready()
-                # await bot.tree.fake_translator()
-                await bot.tree_sync()
+
+                if args.dry_run:
+                    await bot.tree.fake_translator()
+                else:
+                    await bot.tree.sync()
+
                 await bot.close()
 
             await bot.start()
