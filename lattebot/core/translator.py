b5cb99549aa1f9f074eb2cb195b66272533875d3
STACiA <stacia.dev@gmail.com>
2025-12-20 17:18:47 +0700

    chore: enhance is_ready method to check if loading task is done

--- lattebot/core/translator.py
+++ lattebot/core/translator.py
@@ -458,7 +458,12 @@ class Translator(_Translator):
         log.info('unloaded')
 
     def is_ready(self) -> bool:
-        return self._ready.is_set() and self._loading_task is not None and self._loading_task.exception() is None
+        return (
+            self._ready.is_set()
+            and self._loading_task is not None
+            and self._loading_task.done()
+            and self._loading_task.exception() is None
+        )
 
     async def wait_until_ready(self) -> None:
         task = self._loading_task
